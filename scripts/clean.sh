1a06b95d24c74b0a8678f57f2c351be65106e67e
Arda İçöz <ardaicoz@gmail.com>
2026-02-12 18:31:17 +0300

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,28 +1,4 @@
 #!/bin/sh
-#
-#   CLARK, CLAssifier based on Reduced K-mers.
-#
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#   Copyright 2013-2016, Rachid Ounit <rouni001@cs.ucr.edu>
-#   clean.sh: To erase database directories/files generated/downloaded.
-#
-#   Usage:
-#     ./clean.sh --all    Remove entire database directory and all config files
-#     ./clean.sh --reset  Remove built database files but keep references
-#
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	echo "Usage: ./clean.sh [--all|--reset]"
